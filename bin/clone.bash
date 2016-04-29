272365c6b382bee51ab2f8f31429653b9c363954
Christopher Lane Hinson <lane@webperformance.com>
2016-04-29 11:41:20 -0400

    Clone script can just clone every project.

--- bin/clone.bash
+++ bin/clone.bash
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-if [ -d "$1" ]; then
-  echo "$1 already exists."
-else
-  git clone "https://github.com/lane-webperformance/$1.git"
+if [ -z "$1" ]; then
+  git clone "https://github.com/lane-webperformance/legion.git" &&
+  git clone "https://github.com/lane-webperformance/legion-io.git" &&
+  git clone "https://github.com/lane-webperformance/legion-instrument.git" &&
+  git clone "https://github.com/lane-webperformance/legion-metrics.git" &&
+  git clone "https://github.com/lane-webperformance/legion-io-fetch.git" &&
+  exit 0
+
+  exit 1
 fi
+
+if [ -d "$1" ]; then
+  echo "$1 already exists." && exit 0
+else
+  git clone "https://github.com/lane-webperformance/$1.git" && exit 0
+fi
+
+exit 1
